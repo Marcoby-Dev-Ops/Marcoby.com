3c270fef885db5f51074f09e4fd8064bf6459e6c
Chibueze <chibuezeo@spacekrasher.com>
2025-05-16 02:54:14 +0100

    building

--- server/data/createTechCheckTable.js
+++ server/data/createTechCheckTable.js
@@ -1,7 +1,16 @@
 import pool from "../config/db.js";
 
 const createTechCheckTable = async () => {
-	const queryText = `ALTER TABLE techChecks ADD COLUMN referrer TEXT`;
+	const queryText = `CREATE TABLE IF NOT EXISTS techchecks (
+    id SERIAL PRIMARY KEY,
+    fullname VARCHAR(100) NOT NULL, 
+    businessEmail VARCHAR(100) UNIQUE NOT NULL, 
+    companyName VARCHAR(100) NOT NULL, 
+    helpWith TEXT NOT NULL, 
+    teamSize TEXT NOT NULL,
+    referrer TEXT,
+    createdAt TIMESTAMP DEFAULT NOW()
+)`;
 
 	try {
 		await pool.query(queryText);
