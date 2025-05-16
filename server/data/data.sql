cd706634439a36866a2c23c2b93ce38ed3c27281
Chibueze <chibuezeo@spacekrasher.com>
2025-05-16 02:26:38 +0100

    building

--- server/data/data.sql
+++ server/data/data.sql
@@ -1,4 +1,11 @@
 CREATE TABLE IF NOT EXISTS techchecks (
-   ALTER TABLE techChecks ADD COLUMN referrer TEXT;
+    id SERIAL PRIMARY KEY,
+    fullname VARCHAR(100) NOT NULL, 
+    businessEmail VARCHAR(100) UNIQUE NOT NULL, 
+    companyName VARCHAR(100) NOT NULL, 
+    helpWith TEXT NOT NULL, 
+    teamSize TEXT NOT NULL,
+    referrer TEXT,
+    createdAt TIMESTAMP DEFAULT NOW()
 
 )
